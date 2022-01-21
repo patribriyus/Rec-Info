dc88e939a719b568c932451250ffb917f7231b9e
patribriyus <patribriyus@hotmail.com>
2022-01-21 17:59:32 +0100

    Probemos así

--- TrabajoFinal/src/org/apache/lucene/demo/SemanticGenerator.java
+++ TrabajoFinal/src/org/apache/lucene/demo/SemanticGenerator.java
@@ -7,20 +7,32 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Locale;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Node;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.util.FileManager;
 import org.apache.jena.vocabulary.SKOS; 
 import org.apache.jena.vocabulary.DCTerms;
+import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
 
 // Clase que contiene el programa de transformación de la colección XML en RDF
 public class SemanticGenerator {
@@ -29,7 +41,7 @@ public class SemanticGenerator {
 	
 	public static void main(String[] args) throws IOException {
 		String usage = "Uso:\tjava SemanticGenerator "
-						 + "-rdf <rdfPath> " // rdfPath --> debería ser RDFS.ttl
+						 + "-rdf <rdfPath> " // rdfPath --> debería ser RDF.ttl
 						 + "-skos <skosPath> "
 						 + "-owl <owlPath> "
 						 + "-docs <docsPath>\n";
@@ -60,21 +72,19 @@ public class SemanticGenerator {
 		}
 
 		String[] aux = {skosPath, owlPath, docsPath};
-		// TODO: descomentar para cuando se metan los ficheros SKOS y OWL
-		// for(String item : aux){
-		// 	// Check paths
-		// 	final File file = new File(item);
-		// 	if (!file.exists() || !file.canRead()) {
-		// 		System.out.println("El directorio o fichero '" +file.getAbsolutePath()+ "' no existe o no se puede leer.");
-		// 		System.exit(1);
-		// 	}
-		// }		
-		
+		for(String item : aux){
+			// Check paths
+			final File file = new File(item);
+			if (!file.exists() || !file.canRead()) {
+				System.out.println("El directorio o fichero '" +file.getAbsolutePath()+ "' no existe o no se puede leer.");
+				System.exit(1);
+			}
+		}		
 		
 		Date start = new Date();
 		
 		// Generar RDF de la coleccion
-		generateRDF(docsPath);
+		Model collectionModel = generateCollectionModel(docsPath);
 		
 		Date end = new Date();
 		System.out.println((end.getTime() - start.getTime())/1000.0 + " seg");
@@ -82,20 +92,29 @@ public class SemanticGenerator {
 		start = new Date();
 		
 		// Inferencia RDFS
-		// TODO: descomentar para cuando se metan los ficheros SKOS y OWL
-		// inferenciaRDFS(rdfPath, owlPath, skosPath);
+		Model owlModel = RDFDataMgr.loadDataset(RDFDataMgr.open(owlPath).getBaseURI()).getDefaultModel();
+		Model skosModel = RDFDataMgr.loadDataset(RDFDataMgr.open(skosPath).getBaseURI()).getDefaultModel();
+
+		// Estas funciones estan deprecated, no utilizar
+		// Model owlModel =  FileManager.get().loadModel(owlPath,"RDF/XML-ABBREV");
+		// Model skosModel = FileManager.get().loadModel(skosPath,"RDF/XML-ABBREV");
+
+		Model resultModel = ModelFactory.createUnion(ModelFactory.createUnion(collectionModel, owlModel), skosModel);
+		InfModel inf = ModelFactory.createRDFSModel(resultModel);
 		
 		end = new Date();
 		System.out.println((end.getTime() - start.getTime())/1000.0 + " seg");
+
+		FileWriter rdf_file = new FileWriter(rdfPath);
+		inf.write(rdf_file, "TURTLE"); // Creación del RDF.ttl
 	}
 
 	// Adaptación del fichero IndexFiles
-	static void generateRDF(String docsPath) throws IOException{
+	static Model generateCollectionModel(String docsPath) throws IOException{
+
+		Model collectionModel = ModelFactory.createDefaultModel();
 		final File docs_input = new File(docsPath);
 
-		Model model = ModelFactory.createDefaultModel();	
-
-		// do not try to index files that cannot be read
 		if (docs_input.canRead()) {
 			if (docs_input.isDirectory()) {
 				String[] files = docs_input.list();
@@ -103,7 +122,6 @@ public class SemanticGenerator {
 				if (files != null) {
 					for (int i=0; i<files.length; i++) {
 						try {
-
 							// make a new, empty document
 							Document doc = new Document();
 
@@ -151,24 +169,22 @@ public class SemanticGenerator {
 							* Creación de propiedades
 							*/ 
 							
-							String prefix_rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-							// String prefix_rdfs = "http://www.w3.org/2000/01/rdf-schema#";
 							String prefix_mv = "http://www.example.org/#";
 							
 							// Identifier
-							Resource docResource = model.createResource(identifier)
+							Resource docResource = collectionModel.createResource(identifier)
 									.addProperty(DCTerms.identifier, identifier);
 
 							// Title
-							Property titleProperty = model.createProperty(prefix_mv+"title");
+							Property titleProperty = collectionModel.createProperty(prefix_mv+"title");
 							docResource.addProperty(titleProperty, title);
 
 							// Contributor
 							try{
-							Property contributorProperty = model.createProperty(prefix_mv+"contributor");
+							Property contributorProperty = collectionModel.createProperty(prefix_mv+"contributor");
 							for (String contributor : contributors) {
 								contributor = StringUtils.stripAccents(contributor);
-								Resource contributorResource = model.createResource(
+								Resource contributorResource = collectionModel.createResource(
 										StringUtils.stripAccents(contributor));								
 								// A veces el campo del nombre no está separado por ','
 								String firstName = "", familyName = "";
@@ -202,36 +218,37 @@ public class SemanticGenerator {
 							}
 
 							// Subject
-							Property subjectProperty = model.createProperty(prefix_mv+"subject");
+							Property subjectProperty = collectionModel.createProperty(prefix_mv+"subject");
 							for (String subject : subjects) {
 								subject = StringUtils.stripAccents(subject);
-								Resource subjectResource = model.createResource(
+								Resource subjectResource = collectionModel.createResource(
 										StringUtils.stripAccents(subject));
 
 								subjectResource.addProperty(SKOS.prefLabel, subject);
 								docResource.addProperty(subjectProperty, subjectResource);
+
+								// insertSubject(docResource, NS, collectionModel, skosModel, subject);
 							}
 							
-							// TODO: un resource por cada tipo??
-							Property typeProperty = model.createProperty(prefix_rdf+"type");
 							String docType;
 							if (type.contains("tesis")) docType = "tesis";
 							else if (type.contains("tfm")) docType = "tfm";
 							else if (type.contains("tfg")) docType = "tfg";
 							else docType = "pfc";
 							
-							Resource typeResource = model.createResource(prefix_mv+docType);
-							docResource.addProperty(typeProperty, typeResource);
+							// TODO: un resource por cada tipo??
+							Resource typeResource = collectionModel.createResource(prefix_mv+docType);
+							docResource.addProperty(RDF.type, typeResource);
 							
 							// Description
 							if(description != null){
-								Property descriptionProperty = model.createProperty(prefix_mv+"description");
+								Property descriptionProperty = collectionModel.createProperty(prefix_mv+"description");
 								docResource.addProperty(descriptionProperty, StringUtils.stripAccents(description));
 							}
 
 							// Creator
-							Property creatorProperty = model.createProperty(prefix_mv+"creator");
-							Resource creatorResource = model.createResource(creator);
+							Property creatorProperty = collectionModel.createProperty(prefix_mv+"creator");
+							Resource creatorResource = collectionModel.createResource(creator);
 							// A veces el campo del nombre no está separado por ','
 							String firstName = "", familyName = "";
 							if(creator.contains(",")){
@@ -260,33 +277,33 @@ public class SemanticGenerator {
 							docResource.addProperty(creatorProperty, creatorResource);
 
 							// Publisher
-							Property publisherProperty = model.createProperty(prefix_mv+"publisher");
+							Property publisherProperty = collectionModel.createProperty(prefix_mv+"publisher");
 							docResource.addProperty(publisherProperty, publisher);
 
 							// Language
-							Property languageProperty = model.createProperty(prefix_mv+"language");
-							Resource languageResource = model.createResource()
+							Property languageProperty = collectionModel.createProperty(prefix_mv+"language");
+							Resource languageResource = collectionModel.createResource()
 									.addProperty(DCTerms.language, language);
 							docResource.addProperty(languageProperty, languageResource);
 
 							// Date
 							if(date != null){
-								Property dateProperty = model.createProperty(prefix_mv+"date");
-								Resource dateResource = model.createResource()
+								Property dateProperty = collectionModel.createProperty(prefix_mv+"date");
+								Resource dateResource = collectionModel.createResource()
 										.addProperty(DCTerms.date, date);
 								docResource.addProperty(dateProperty, dateResource);
 							}
 
 							// Relation
-							Property relationProperty = model.createProperty(prefix_mv+"relation");
-							Resource relationResource = model.createResource(relation);
+							Property relationProperty = collectionModel.createProperty(prefix_mv+"relation");
+							Resource relationResource = collectionModel.createResource(relation);
 							relationResource.addProperty(DCTerms.relation, relation);
 							docResource.addProperty(relationProperty, relationResource);
 
 							// Rights
 							if(rights != null){
-								Property rightsProperty = model.createProperty(prefix_mv+"rights");
-								Resource rightsResource = model.createResource(rights);
+								Property rightsProperty = collectionModel.createProperty(prefix_mv+"rights");
+								Resource rightsResource = collectionModel.createResource(rights);
 								rightsResource.addProperty(DCTerms.rights, rights);
 								docResource.addProperty(rightsProperty, rightsResource);
 							}
@@ -295,19 +312,15 @@ public class SemanticGenerator {
 							e.printStackTrace();
 						}
 					}
-
-					FileWriter ttl_output = new FileWriter("RDF.ttl");
-					model.write(ttl_output, "TURTLE"); // Se escribe en formato turtle
-					// FileWriter rdf_output = new FileWriter("RDF.rdf");
-					// model.write(rdf_output, "RDF/XML-ABBREV"); // Se escribe formato rdf/xml
 				}
-			}				
+			}
 		}
+
+		return collectionModel;
 	}
 
 	private static void AddAllFields(Document doc, org.w3c.dom.Document docTree) {
-		NodeList nodeList;
-		nodeList = docTree.getElementsByTagName("*");
+		NodeList nodeList = docTree.getElementsByTagName("*");
 		for (int i=0; i<nodeList.getLength(); i++) {
 			org.w3c.dom.Node node = nodeList.item(i);
 			if (node.getNodeType() == Node.ELEMENT_NODE) {
@@ -315,18 +328,37 @@ public class SemanticGenerator {
 			}
 		}
 	}
-	  
-	static void inferenciaRDFS(String rdfPath, String owlPath, String skosPath) throws IOException {
-		// TODO: así se cargan los models de tipo ttl y owl?
-		Model rdfModel = RDFDataMgr.loadDataset(RDFDataMgr.open("RDF.ttl").getBaseURI()).getDefaultModel();
-		Model owlModel = RDFDataMgr.loadDataset(RDFDataMgr.open(owlPath).getBaseURI()).getDefaultModel();
-		Model skosModel = RDFDataMgr.loadDataset(RDFDataMgr.open(skosPath).getBaseURI()).getDefaultModel();
-		
-		Model resultModel = ModelFactory.createUnion(ModelFactory.createUnion(rdfModel, owlModel), skosModel);
-		InfModel inf = ModelFactory.createRDFSModel(resultModel);
-		
-		FileWriter rdfs_file = new FileWriter(rdfPath);
-		inf.write(rdfs_file, "TURTLE"); // Creación del RDFS.ttl
-	}
 
+// 	private void insertSubject(Resource document, String NS, Model model, Model thesaurus, String s) {
+// 		s= s.replace("'", "");
+// 		s= s.replace("\n", "").toLowerCase(Locale.ROOT);
+// 		String queryString =
+// 				  "prefix skos: <http://www.w3.org/2004/02/skos/core#>" +
+// 							 "prefix s: <http://www.grupo202.com/skos/subjects#>" +
+// 							 "select ?parentConcept ?parentLabel WHERE {" +
+// 								  "{ ?childConcept skos:prefLabel '" + s + "' } . " +
+// 								  "{ ?childConcept skos:broader ?parentConcept } . " +
+// 								  "{?parentConcept skos:prefLabel ?parentLabel} " +
+// 							 "}";
+
+// 		Query q1 = QueryFactory.create(queryString) ;
+
+// 		try (QueryExecution qexec = QueryExecutionFactory.create(q1, thesaurus)) {
+// 			 ResultSet results = qexec.execSelect();
+// 			 while (results.hasNext()) {
+// 				  QuerySolution soln = results.nextSolution();
+// 				  Resource x = soln.getResource("parentConcept");
+// 				  RDFNode z = soln.get("parentLabel");
+
+// 				  Resource newResource = model.createResource(SKOS.Concept);
+// 				  Resource resourceToInsert = model.createResource(NS + x.getURI());
+// 				  resourceToInsert.addProperty(RDF.type, newResource);
+
+// 				  resourceToInsert.addProperty(SKOS.prefLabel, model.createLiteral(z.toString().toLowerCase(Locale.ROOT)));
+// 				  document.addProperty(model.getProperty(NS + SUBJECT_ID), resourceToInsert);
+
+
+// 			 }
+// 		}
+//   }
 }
